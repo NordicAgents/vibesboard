5bfc66186c613f1fa51b169ac8498fd51227319b
midhun xavier <midhunxavier@outlook.com>
2026-05-25 16:24:21 +0200

    feat(auth): RISC-effect helpers (resolve google sub, revoke sessions, set disabled)

--- packages/adapter-better-auth/src/index.ts
+++ packages/adapter-better-auth/src/index.ts
@@ -1,3 +1,8 @@
 export { auth, type Auth } from './config.ts'
 export { onUserCreateAfter } from './on-user-create.ts'
 export { sendMagicLinkEmail, sendVerifyEmail, sendResetPasswordEmail } from './email.ts'
+export {
+  resolveUserIdByGoogleSub,
+  revokeUserSessions,
+  setUserDisabled,
+} from './risc-effects.ts'
